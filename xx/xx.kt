5e0d151bd7b39c0d3b4cb00bdd640756a80bd932
ParkJong-Hun <thesn515@gmail.com>
2022-02-01 13:33:42 +0900

    xx

--- xx/xx.kt
+++ xx/xx.kt
@@ -1,7 +1,18 @@
-fun main(x: Int, n: Int): LongArray {
+fun main(args: Array<String>) {
+    val (a,b) = readLine()!!.split(' ').map { it.toInt() }
+    val answer = solution(a, b)
+    print("[")
+    for (i in answer) {
+        print("$i, ")
+    }
+    print("]")
+}
+
+fun solution(x: Int, n: Int): LongArray {
     return Array<Long>(n){i -> (x.toLong() * (i + 1))}.toLongArray()
     //Array를 Long타입으로 생성. 
     //i는 0부터 1씩 증가.
     //x.toLong() * (i + 1)을 Array에 추가
-    //위의 내용을 n만큼 반복. 
+    //위의 내용을 n만큼 반복.
+    //Array<Long>을 LongArray로 변환 
 }
