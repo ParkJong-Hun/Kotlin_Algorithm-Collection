509846a9331bbf8dba2c3919f6d5cda227d8d5ff
ParkJong-Hun <thesn515@gmail.com>
2022-02-24 14:30:39 +0900

    2022/02/24 14:30 collatz

---
+++ collatz/collatz.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println(solution(6))//8
+    println(solution(16))//4
+    println(solution(626331))//-1
+}
+
+fun solution(num: Int): Int {
+    var count = 0
+    var result = 0
+    var temp = num.toLong()
+    while(true) {
+        count++
+        if(count >= 500) {
+            return -1
+        } else if(temp == 1.toLong()) {
+            return result
+        }
+        if(temp % 2 == 0.toLong()) {
+            temp /= 2
+        } else {
+            temp = temp * 3 + 1
+        }
+        result++
+    }
+}
+//tailrec
